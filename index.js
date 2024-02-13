ba3866432375e4682ae4ecd9d649789ff600e821
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-13 20:30:28 +0300

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('Hello, teacher');
+const goods = 'box';
+const quantity = 325;
+const category = 'books';
+let price = 1111;
+console.log('box');
+console.log(quantity*price);
