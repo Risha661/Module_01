c6feb87a4b0b50c79e0ae59090fba58113cf43c5
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-13 23:50:03 +0300

    Внесли вторую категорию товаров в index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
-console.log('Hello, teacher');
-const goods = 'box';
-const quantity = 325;
-const category = 'books';
-let price = 1111;
-console.log('box');
-console.log(quantity*price);
+{
+    console.log('Hello, teacher');
+    const goods = 'box';
+    const quantity = 325;
+    const category = 'books';
+    let price = 1111;
+    console.log('box');
+    console.log(quantity * price);
+}
+
+{
+    const item = 'pencil';
+    const count = 1000;
+    const category = 'stationery';
+    let secondPrice = 50;
+    console.log(item);
+    console.log(`Сумма канцелярских товаров:${count * secondPrice}`);
+}
