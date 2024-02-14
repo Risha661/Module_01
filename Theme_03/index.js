edaa3392ca3d77dbb3c8258690940633c544ed9d
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-14 21:36:45 +0300

    Решена 1-ая задача 3-й темы в 1 модуле

---
+++ Theme_03/index.js
@@ -0,0 +1,33 @@
+
+'use strict';
+/*
+{
+    console.log('Hello, teacher');
+    const goods = 'box';
+    const quantity = 325;
+    const category = 'books';
+    let price = 1111;
+    console.log('box');
+    console.log(quantity * price);
+}
+
+{
+    const item = 'pencil';
+    const count = 1000;
+    const category = 'stationery';
+    let secondPrice = 50;
+    console.log(item);
+    console.log(`Сумма канцелярских товаров:${count * secondPrice}`);
+}
+*/
+
+{
+    const items = prompt('Введите наименование товара');
+
+    const count = Number(prompt('Введите количество товара'));
+    const category = prompt('Введите категорию товара');
+    let secondPrice = prompt('Укажите цену на товар') / 1;
+    console.log('тип переменной \"количество\"' + typeof(count));
+    console.log('тип переменной \"цена\"' + typeof(secondPrice));
+    console.log('На складе ' + count + ' единицы товара ' + items + ' на сумму ' + secondPrice + ' деревянных');
+}
