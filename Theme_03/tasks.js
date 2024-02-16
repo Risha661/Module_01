9482edfdda53ed8e920d4212d856dfd8b49ed9a3
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-16 18:35:50 +0300

    Добавлен tasks.js и 2-я задача из обязательных

---
+++ Theme_03/tasks.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+    const rain = Math.round(Math.random());
+    console.log (rain);
+    if (rain === 1) {
+        console.log('Пошёл дождь. Возьмите зонт!')
+    } else if (rain === 0) {
+        console.log('Дождя нет!')
+    }
+}
