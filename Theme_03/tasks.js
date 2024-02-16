05d56c6fb0abdb1bb7a8f97e839384aabb251d26
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-16 19:23:59 +0300

    Добавлена 3-я задача

--- Theme_03/tasks.js
+++ Theme_03/tasks.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 {
     const rain = Math.round(Math.random());
     console.log (rain);
@@ -8,3 +9,23 @@
         console.log('Дождя нет!')
     }
 }
+*/
+
+{
+    const mathematics = prompt('Введите кол-во баллов по математике:');
+    const russianLanguage = prompt('Введите кол-во баллов по русскому языку:');
+    const informatics = prompt('Введите кол-во баллов по информатике');
+    let totalPoints = mathematics + russianLanguage + informatics;
+    if (totalPoints >= 265) {
+        console.log('Поздравляю, вы поступили на бюджет!');
+        alert('Поздравляю, вы поступили на бюджет!');
+    }
+}
+    /*
+    switch (totalPoints) {
+        case mathematics >= 85:
+        case russianLanguage >= 75:
+        case informatics >= 95:
+        break;
+    }
+    Данное решение не подходит, но сохранено на будущее */
