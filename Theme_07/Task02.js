bda0f11d63a7c03a99c0d1894cee86a7788ca05c
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-20 09:24:12 +0300

    2 task

--- Theme_07/Task02.js
+++ Theme_07/Task02.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+
+const getAverageValue = (allСashbox) => {
+    let sum = 0; // сумма всех чисел массива
+    for (let i = 0; i < allСashbox.length; i += 1) { // цикл
+        sum += allСashbox[i];
+    }
+    return sum / allСashbox.length; // возвращаем среднее арифметическое
+}
+
+console.log(getAverageValue(allСashbox)); 
