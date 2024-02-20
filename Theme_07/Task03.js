eae0e6662055fa723b1791ea9230ab3a08bbaf30
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-02-20 19:46:47 +0300

    Правки в 3 task

--- Theme_07/Task03.js
+++ Theme_07/Task03.js
@@ -1,9 +1,18 @@
 'use strict';
 
+// const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+// const prefix = 'Mr';
+
+// const addPrefix = (names, 'Mr') => {
+//     const newPreArr = names.map(n => prefix + ' ' + n); // Возвращаем измененный массив с новыми переменными
+//     return newPreArr;
+// }
+// console.log(addPrefix(names, prefix));
+
 const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
 const prefix = 'Mr';
 
-const addPrefix = (names, prefix) => {
+function addPrefix(names, prefix) {
     const newPreArr = names.map(n => prefix + ' ' + n); // Возвращаем измененный массив с новыми переменными
     return newPreArr;
 }
