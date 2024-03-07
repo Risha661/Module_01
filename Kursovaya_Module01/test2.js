6e5f9a3a3ec8e9e1155826ce8937220930fd0706
Ирина Князева <knyazeva.irina.94@mail.ru>
2024-03-07 13:00:15 +0300

    Тестовый коммит для разбора ошибок

--- Kursovaya_Module01/test2.js
+++ Kursovaya_Module01/test2.js
@@ -2,17 +2,17 @@
 (() => {
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
   const varNum = ['четное', 'нечетное'];
-
+ 
   const getRandomIntInclusive = (min, max) => {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
   };
-
+ 
   const isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
-  };
-
+};
+ 
   const game = (language) => {
     const result = {
       player: 0,
@@ -21,10 +21,10 @@
       computerAmount: 5,
       compHandsGuess: 0,
       playerHandGuess: 0,
-
+      currentTry: 0,               
     };
-
-    function wannaExit() {
+ 
+    function wannaExit(){
       const userAnswer = confirm('Точно ли Вы хотите выйти?');
       if (userAnswer) {
         alert('Игра окончена!');
@@ -33,20 +33,22 @@
         return false;
       }
     }
-
+ 
     return function start() {
-
+ 
       function tryEndGame() {
-        const userTryEnd = alert('Хотите сыграть еще? :)');
+        const userTryEnd = confirm('Хотите сыграть еще? :)');
         if (userTryEnd) {
           start();
+        }  else {
+          return true;
         }
       }
 
       function compGuess() {
         result.compHandsGuess = getRandomIntInclusive(1, result.computerAmount);
         console.log('На момент хода бота у игрока ' +
-          result.playerAmount + ' шариков, у бота - ' + result.computerAmount);
+         result.playerAmount + ' шариков, у бота - ' + result.computerAmount);
         const playerTry = confirm('Число четное?');
         console.log('Бот загадал ' + result.compHandsGuess + ' шариков');
         if ((playerTry === true && (result.compHandsGuess % 2 === 0)) ||
@@ -64,41 +66,43 @@
         }
         if (result.playerAmount > 0) {
           if (result.computerAmount > 0) {
-            return userGuess();
+            result.currentTry = 0;
+            return false;
           } else {
             alert('Вы выиграли. Счет: ' +
-              maxBallsValue + ' шариков. У бота 0 шариков.');
-            return tryEndGame();
+            maxBallsValue + ' шариков. У бота 0 шариков.');
           }
         } else {
           alert('Бот выиграл. Счет: ' +
-            maxBallsValue + ' шариков. У вас 0 шариков.');
-          return tryEndGame();
+          maxBallsValue + ' шариков. У вас 0 шариков.');
         }
+        return tryEndGame();
+
       }
 
       function userGuess() {
         console.log('На момент хода игрока у бота ' +
-          result.computerAmount + ' шариков, у игрока - ' + result.playerAmount);
+         result.computerAmount + ' шариков, у игрока - ' + result.playerAmount);
         result.playerHandGuess = prompt('Выберите количество' +
-          ' шариков от 1 до ' + result.playerAmount);
-
-        if (result.playerHandGuess == null) {
-          if (wannaExit()) {
-            return true;
-          } else {
-            return userGuess();
-          }
-        }
-        if (!isNumber(result.playerHandGuess)) {
-          alert('Введите корректное число!');
-          return userGuess();
-        } else if (result.playerHandGuess < 1 || result.playerHandGuess > result.playerAmount) {
-          alert('Введите корректное в диапазоне: от 1 до ' + result.playerAmount);
-          return userGuess();
+         ' шариков от 1 до ' + result.playerAmount);
+  
+      if (result.playerHandGuess == null){
+        let a = wannaExit();
+        if (a){
+          return true;
         } else {
-          result.playerHandGuess = Number(result.playerHandGuess);
+          return userGuess();
         }
+      }
+      if (!isNumber(result.playerHandGuess)) {
+        alert('Введите корректное число!');
+        return userGuess();
+      } else if (result.playerHandGuess < 1 || result.playerHandGuess > result.playerAmount) {
+        alert('Введите корректное в диапазоне: от 1 до ' + result.playerAmount);
+        return userGuess();
+      } else {
+        result.playerHandGuess = Number(result.playerHandGuess);
+      }
         console.log('Игрок загадал ' + result.playerHandGuess + ' шариков');
         const botTry = varNum[getRandomIntInclusive(0, 1)];
         console.log('Бот выбрал - ' + botTry);
@@ -109,32 +113,31 @@
           result.playerAmount -= result.playerHandGuess;
           result.playerHandGuess = 0;
           console.log('Бот выиграл. у него шариков ' +
-            result.computerAmount + ' у игрока - ' + result.playerAmount);
+           result.computerAmount + ' у игрока - ' + result.playerAmount);
         } else if ((botTry === 'четное' &&
-          (result.playerHandGuess % 2 !== 0)) ||
+         (result.playerHandGuess % 2 !== 0)) ||
           (botTry === 'нечетное' && (result.playerHandGuess % 2 === 0))) {
           alert('Вы выиграли! Бот проиграл этот раунд!');
           result.computerAmount -= result.playerHandGuess;
           result.playerAmount += result.playerHandGuess;
           result.playerHandGuess = 0;
           console.log('Вы выиграли. у бота шариков ' +
-            result.computerAmount + ' у игрока - ' + result.playerAmount);
+           result.computerAmount + ' у игрока - ' + result.playerAmount);
         }
         if (result.computerAmount > 0) {
           if (result.playerAmount > 0) {
-            return compGuess();
+            result.currentTry = 1;
+            return false;
           } else {
             alert('Бот выиграл. Счет: ' +
-              maxBallsValue + ' шариков. У вас 0 шариков.');
-            return tryEndGame();
+            maxBallsValue + ' шариков. У вас 0 шариков.');
           }
         } else {
           alert('Вы выиграли. Счет: ' +
-            maxBallsValue + ' шариков. У бота 0 шариков.');
-          return tryEndGame();
+          maxBallsValue + ' шариков. У бота 0 шариков.');
         }
+        return tryEndGame();
       }
-
 
       result.playerAmount = 5;
       result.computerAmount = 5;
@@ -144,8 +147,9 @@
       const botChange = FIGURES_RUS[getRandomIntInclusive(0, 2)];
       let userChange = prompt('Камень, ножницы, бумага?');
       if (userChange === null) {
-        if (wannaExit()) {
-          return;
+        let a = wannaExit();
+        if (a){
+          return true;
         } else {
           start();
         }
@@ -162,28 +166,39 @@
         start();
       }
       if (userChange === botChange) {
-        alert('У вас с ботом ничья! Давайте заново!');
-        start();
+        confirm('У вас с ботом ничья! Давайте заново!');
+        return start();
       }
       if ((userChange === 'камень' && botChange === 'ножницы') ||
         (userChange === 'ножницы' && botChange === 'бумага') ||
         (userChange === 'бумага' && botChange === 'камень')) {
-        alert('Вы выиграли! В игре "Марблы" первый ход ваш!');
-        ++result.player;
+        confirm('Вы выиграли! В игре "Марблы" первый ход ваш!');
+        result.currentTry = 0;
       } else {
-        alert('Бот выиграл! В игре "Марблы" первый ход за ним!');
-        ++result.computer;
+        confirm('Бот выиграл! В игре "Марблы" первый ход за ним!');
+        result.currentTry = 1;
       }
-      if (result.player === 1) {
-        result.player = false;
-        return userGuess();
+ 
+      let a;
+      let b;
+      if (result.currentTry == 0) {
+        a = userGuess();
       } else {
-        result.computer = false;
-        return compGuess();
+        b = compGuess(); 
+      }
+
+      while (!a || !b){   
+        if(a || b){
+          break;
+        }
+        if (result.currentTry == 1){
+          b = compGuess();
+        } else if (result.currentTry == 0){
+          a = userGuess();
+        }
       }
     };
   };
   window.RPS = game;
 })();
-
-
+ 
